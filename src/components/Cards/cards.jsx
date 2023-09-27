8c4d445d29d9c4dfd6d337f41f157208835b4872
JoaquinRouge <Joarouge@gmail.com>
2023-09-27 19:07:59 -0300

    finished project

--- src/components/Cards/cards.jsx
+++ src/components/Cards/cards.jsx
@@ -1,31 +1,31 @@
 import './cards.css'
 import InfoCards from '../InfoCards/infocards';
-import  {useState} from 'react'
+import { useState } from 'react'
 
-function Cards({data}) {
-        const [Show,setShow]= useState(false)
-        const mostrarLista = ()=>{
-            setShow(true)
-        }
+function Cards({ data }) {
+    const [Show, setShow] = useState(false)
+    const mostrarLista = () => {
+        setShow(true)
+    }
 
 
-    return ( 
+    return (
         <section className='Cards'>
             <div id='CardContainer' className='CardContainer'>
                 <div className="card">
                     <img src={data.image} alt="imagen-personaje" />
                     <h3 id='h3-card'>{data.name}</h3>
                     <div className="button-card">
-                        <button className={Show?'d-none':'d-block'} id='btn-card' onClick={()=> setShow(true)}>Know More...</button>  
+                        <button className={Show ? 'd-none' : 'd-block'} id='btn-card' onClick={() => setShow(true)}>Know More...</button>
                     </div>
                 </div>
                 <div className="infocards">
-                     {Show === true?<InfoCards status={data.status} specie={data.species} origin={data.origin.name} gender={data.gender} setShow={setShow}/>:''}
+                    {Show === true ? <InfoCards status={data.status} specie={data.species} origin={data.origin.name} gender={data.gender} setShow={setShow} /> : ''}
                 </div>
-            </div>   
+            </div>
         </section>
 
-     );
+    );
 }
 
 export default Cards;
